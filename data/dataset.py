797f614ce1b6491fc108b16a3d7368877c494e38
iria <ShamovIA@yandex.ru>
2020-06-06 13:35:55 +0300

    add dataset file

---
+++ data/dataset.py
@@ -0,0 +1,53 @@
+import numpy as np
+import os
+from tqdm import tqdm
+import torch
+import torchaudio
+
+
+class SpeechDataset(torch.utils.data.Dataset):
+    def __init__(self, root):
+        self.root = root
+        self.speakers, self.pathes = SpeechDataset.get_s2u(root)
+        self.speaker_ixs = self.get_onehot_mapping(list(set(self.speakers)))
+
+
+    def get_onehot_mapping(self, categories):
+        return {cat: i for i, cat in enumerate(sorted(categories))}
+
+
+    @staticmethod
+    def get_s2u(root):
+        speakers = []
+        pathes = []
+
+        for path, dirs, files in os.walk(root):
+            speaker = '/'.join(path.split('/')[-2:])
+            files = list(filter(lambda s: s.endswith('.flac'), files))
+            if len(files) > 0:
+                speakers += [speaker for fname in files]
+                pathes += list(map(lambda s: '/%s/%s' % (speaker, s), files))
+                
+        for i in tqdm(range(len(pathes))):
+            utter, bitrate = torchaudio.load(root + pathes[i])
+            if utter.size(1) < 20480:
+                speakers[i] = None
+                pathes[i] = None
+        speakers = list(filter(lambda s: s is not None, speakers))
+        pathes = list(filter(lambda s: s is not None, pathes))
+
+        return speakers, pathes
+
+
+    def __len__(self):
+        return len(self.pathes)
+
+
+    def __getitem__(self, ix):
+        speaker = self.speakers[ix]
+        speaker_ix = self.speaker_ixs[speaker]
+        path = self.root + self.pathes[ix]
+        waveform, bitrate = torchaudio.load(path)
+        if bitrate != 16000: print('not 16k bitrate!')
+        index = np.random.randint(waveform.size(1) - 20480 + 1)
+        return speaker_ix, waveform.squeeze(0)[index:index+20480]
